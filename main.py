4a5fc3d4fc85215c50c41051089c7cecb8d45f93
Sergey Popov <167248754+duckduckduck1@users.noreply.github.com>
2026-07-28 23:52:45 +0400

    Добавить вывод сессий, файлов и времени создания

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from dataclasses import dataclass, field
+from datetime import UTC, datetime
 
 
 @dataclass
-class File:
+class Study:
     id: int
-    session_id: int
     name: str
-    size: int
+    description: str
+    sessions: list = field(default_factory=list)
 
 
 @dataclass
@@ -15,15 +16,17 @@ class Session:
     study_id: int
     note: str
     uploader: str
-    files: list[File] = field(default_factory=list)
+    created_at: datetime
+    files: list = field(default_factory=list)
 
 
 @dataclass
-class Study:
+class File:
     id: int
+    session_id: int
     name: str
-    description: str
-    sessions: list[Session] = field(default_factory=list)
+    size: int
+    created_at: datetime
 
 
 study = Study(
@@ -37,13 +40,34 @@ session = Session(
     study_id=study.id,
     note="Контроль ЭЭГ группа 1 Контроль",
     uploader="Sergey Popov",
+    created_at=datetime.now(UTC),
 )
 
 file = File(
-    id=1, session_id=session.id, name="Архив 10 мышей ЭЭГ Контроль.zip", size=1024
+    id=1,
+    session_id=session.id,
+    name="Архив 10 мышей ЭЭГ Контроль.zip",
+    size=1024,
+    created_at=datetime.now(UTC),
 )
 
-session.files = [file]
-study.sessions = [session]
+session_2 = Session(
+    id=2,
+    study_id=study.id,
+    note="Контроль ЭЭГ группа 2 Контроль",
+    uploader="Sergey Popov",
+    created_at=datetime.now(UTC),
+)
 
-print(study)
+study.sessions.append(session)
+study.sessions.append(session_2)
+session.files.append(file)
+
+for i, s in enumerate(study.sessions):
+    print(
+        f"ID:[{i}] Session ID: {s.id}, Note: {s.note}, Uploader: {s.uploader}, created_at: {s.created_at}"
+    )
+    for j, f in enumerate(s.files):
+        print(
+            f"\tID:[{j}] File ID: {f.id}, session_id: {f.session_id}, Name: {f.name}, Size: {f.size}, created_at: {f.created_at}"
+        )
